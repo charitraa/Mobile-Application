14ef2f29f8b3c4e6e9842f97af14480d27519888
charitraa <stharabi9862187405@gmail.com>
2024-10-27 08:25:20 +0545

    added write in firebase

--- test2/lib/main.dart
+++ test2/lib/main.dart
@@ -5,8 +5,10 @@ import 'package:test2/pages/mapclass.dart';
 import './pages/ChatApp/loginpage.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
-void main() {
-  Firebase.initializeApp(
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized(); // Ensures Flutter is fully initialized
+  await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
   runApp(const MyApp());
@@ -15,33 +17,16 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: Login(),
+      home: LoginWidget(), // Ensure Login is a Widget class
     );
   }
 }
-
--- test2/lib/pages/ChatApp/loginpage.dart
+++ test2/lib/pages/ChatApp/loginpage.dart
@@ -1,6 +1,35 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:test2/pages/ChatApp/Home.dart';
-class Login extends StatelessWidget {
+
+class LoginWidget extends StatefulWidget {
+  const LoginWidget({super.key});
+
+  @override
+  State<LoginWidget> createState() => Login();
+}
+
+class Login extends State<LoginWidget> {
+   TextEditingController nameController = TextEditingController();
+
+  void gotoChatPage() {
+    Navigator.pushAndRemoveUntil(
+      context,
+      MaterialPageRoute(builder: (context) => HomePage()),
+          (route) => false,
+    );
+  }
+
+  readfromstorage() async{
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    String? userid = await prefs.getString('userid');
+    String? username = await prefs.getString('username');
+    if (userid!=null && username!= null){
+      gotoChatPage();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,13 +42,11 @@ class Login extends StatelessWidget {
             Container(
               height: 400,
               color: Colors.pink,
-              child: Icon(Icons.bike_scooter_outlined, size: 200,color: Colors.white,),
+              child: Icon(Icons.bike_scooter_outlined, size: 200, color: Colors.white),
             ),
-
             SizedBox(height: 40),
-
-
             TextField(
+              controller: nameController,
               decoration: InputDecoration(
                 hintText: 'Enter your name',
                 border: OutlineInputBorder(
@@ -28,17 +55,23 @@ class Login extends StatelessWidget {
               ),
             ),
             SizedBox(height: 40),
-
-
             ElevatedButton(
-              onPressed: (){
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => HomePage()),
-                  );
+              onPressed: () async {
+                if (nameController.text.isNotEmpty) {
+                  String userId = DateTime.now().toIso8601String();
+                  FirebaseFirestore.instance.collection('USER').doc(userId).set({''
+                      'name':nameController.text,
+                  });
+                  final SharedPreferences prefs = await SharedPreferences.getInstance();
+                  await prefs.setString('userid', userId);
+                  await prefs.setString('username', nameController.text);
+
+                  gotoChatPage();
+                }
+
               },
               style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.pinkAccent, // Background color
+                backgroundColor: Colors.pinkAccent,
                 padding: EdgeInsets.symmetric(vertical: 15),
                 shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(10),
@@ -52,8 +85,7 @@ class Login extends StatelessWidget {
                     style: TextStyle(fontSize: 18, color: Colors.white),
                   ),
                   SizedBox(width: 10),
-                  Icon(Icons.arrow_forward, color: 
-                    Colors.white,),
+                  Icon(Icons.arrow_forward, color: Colors.white),
                 ],
               ),
             ),
