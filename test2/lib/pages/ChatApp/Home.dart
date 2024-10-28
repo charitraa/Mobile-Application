fd73a1384a74b0154387d203e2302ede91354d48
charitraa <stharabi9862187405@gmail.com>
2024-10-28 11:02:30 +0545

    get the api and show in gui

--- test2/lib/pages/ChatApp/Home.dart
+++ test2/lib/pages/ChatApp/Home.dart
@@ -2,6 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:test2/model/chartmodel.dart';
 import 'package:test2/pages/ChatApp/Conversation.dart';
+import 'package:test2/pages/ChatApp/api/datalayer.dart';
 import 'package:test2/static.dart';
 
 class HomePage extends StatefulWidget {
@@ -13,46 +14,44 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   // Corrected function name from ConverstaionItem to ConversationItem
-  Widget ConversationItem(Size) {
+  conversationitem(var size, ConversationModel data){
     return GestureDetector(
-      onTap: () {
-        // Navigate to the Conversation screen when tapped
-        Conversation();
+      onTap: (){
+        // chat
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) =>  Conversation()),
+        );
       },
-      child: Row(
-        children: [
-          Container(
-            width: 80,
-            height: 80,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(100),
+      child: Container(
+        width: size.width,
+        margin: EdgeInsets.only(bottom: 15),
+        child: Row(
+          children: [
+            Container(
+              width: 80,
+              height: 80,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(100)
+              ),
+              margin: EdgeInsets.only(right: 15,left: 15),
+              child: ClipRRect(
+                  borderRadius: BorderRadius.circular(100),
+                  child: Image.network(StaticValue.baseURL,height: 80,width: 80,
+                    fit: BoxFit.cover,)),
             ),
-            margin: EdgeInsets.only(right: 15, left: 15),
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(100),
-              child: Image.network(
-                StaticValue.baseURL, // Ensure this URL is valid
-                height: 80,
-                width: 80,
-                fit: BoxFit.cover,
-              ),
-            ),
-          ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start, // Align text to the start
-            children: [
-              Text(
-                "name", // You might want to replace "name" with a dynamic value
-                style: TextStyle(color: Colors.black, fontSize: 18),
-              ),
-              SizedBox(height: 10),
-              Text(
-                "datetime", // You might want to replace "datetime" with a dynamic value
-                style: TextStyle(color: Colors.grey, fontSize: 20),
-              ),
-            ],
-          ),
-        ],
+
+            //name
+            Column(
+              children: [
+                Text(data.name,style: TextStyle(color: Colors.black,fontSize: 18),),
+                SizedBox(height: 10,),
+                Text(data.datetime!,style: TextStyle(color: Colors.grey,fontSize: 12),)
+              ],
+            )
+
+          ],
+        ),
       ),
     );
   }
@@ -80,30 +79,65 @@ class _HomePageState extends State<HomePage> {
   void initState() {
     // TODO: implement initState
     super.initState();
-    addConversation();
+    // addConversation();
+    futureconversationdata = Datalayer.getConversations();
   }
 
   @override
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
-
     return Scaffold(
-      appBar: AppBar(),
-      body: Column(
-        children: [
-          Container(
-            width: size.width,
-            height: size.height / 1.2,
-            child: ListView.builder(
-              scrollDirection: Axis.vertical,
-              itemCount: 5,
-              itemBuilder: (context, index) {
-                return ConversationItem(size);
-              },
-            ),
+
+        body:  SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(height: 55,),
+              Text("Random Chat Application",style: TextStyle(color: Colors.black,fontSize: 20),),
+
+              FutureBuilder(
+                future: futureconversationdata,
+                builder: (context, AsyncSnapshot<dynamic> snapshot) {
+                  switch (snapshot.connectionState) {
+                    case ConnectionState.none:
+                    // network, server error
+                      return Container(
+                        height: size.height,
+                        width: size.width, child:
+                      const Center(child: Text("Server Error")),);
+                    case ConnectionState.active:
+                      return const CircularProgressIndicator();
+                    case ConnectionState.waiting:
+                      return const CircularProgressIndicator();
+                    case ConnectionState.done:
+                      List<ConversationModel>? dataa = snapshot.data;
+                      return (snapshot.data != null || snapshot.hasData)?
+                      Container(
+                        width: size.width,
+                        height: size.height/1.1,
+                        child: ListView.builder(
+                          scrollDirection: Axis.vertical,
+                          itemCount: dataa!.length,
+                          itemBuilder: (context, index) {
+                            return conversationitem(size,dataa[index]);
+                          },
+                        ),
+                      ):Container(
+                        height: size.height/1.2,
+                        width: size.width, child:
+                      const Center(child: Text("No Data Available")),);
+                    default:
+                      return Container(
+                        height: size.height,
+                        width: size.width, child:
+                      const Center(child: Text("Server Error")),);
+
+                  }
+                },
+              ),
+
+            ],
           ),
-        ],
-      ),
+        )
     );
   }
 }
